5170780172316455e9d1b80fb680293555c4e420
Cengiz YILMAZ <terabilgi@gmail.com>
2022-08-27 01:22:21 +0300

    ToogleTheme oluşturuluyor

--- client/components/ToggleTheme.jsx
+++ client/components/ToggleTheme.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react"
+import { ThemeContext } from "../context/theme"
+
 const ToggleTheme = () => {
     return (
       <>
         <span style={{ fontSize: '2em' }}>🌞</span>
-        <span style={{ fontSize: '2em' }}>🌓</span>
+        <span style={{ fontSize: '2em ' }}>🌓</span>
       </>
     )
 }
